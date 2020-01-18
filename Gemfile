c0783a54cefd46c3e35ae42e8ae52ba01b99f863
Leonid Smirnov <leonid.smirnov@uchi.ru>
2020-01-18 18:56:59 +0300

    Added the returning some properties data.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,11 @@ source 'https://rubygems.org'
 
 gem 'sinatra', github: 'sinatra/sinatra'
 
-gem 'sinatra-activerecord'
 gem 'pg'
+gem 'activerecord'
+gem 'sinatra-activerecord'
 gem 'rake'
+
+group :development do
+  gem 'pry'
+end
--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'sinatra/activerecord'
 
-get '/' do
-  "Hello, world!"
+current_dir = Dir.pwd
+Dir["#{current_dir}/models/*.rb"].each { |file| require file }
+
+before do
+  content_type 'application/json'
+end
+
+get '/stats' do
+  @properties = Property.first(100)
+
+  @properties.map do |property|
+    property.offer_type
+  end.to_json
 end
