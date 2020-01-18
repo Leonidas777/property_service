e1d9da70c72a39481864c6dc6babd8d7487c86ea
Leonid Smirnov <leonid.smirnov@uchi.ru>
2020-01-18 19:01:16 +0300

    Added some more info.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'pry'
 
 current_dir = Dir.pwd
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
@@ -11,7 +12,9 @@ end
 get '/stats' do
   @properties = Property.first(100)
 
+  # binding.pry
+
   @properties.map do |property|
-    property.offer_type
+    property.slice(:offer_type, :property_type, :street, :lng, :lat, :construction_year, :price)
   end.to_json
 end
